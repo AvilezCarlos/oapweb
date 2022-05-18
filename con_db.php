c83a8f918662cff02391e06810d54bc41dcc3f49
Avilez Carlos <band_rockstar@hotmail.com>
2022-05-18 18:42:21 -0500

    Miercoles 18/05 2

--- con_db.php
+++ con_db.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-$conex = mysqli_connect("localhost","root","","registro"); 
-=======
 $conn = mysqli_connect("localhost","root","","oap");
->>>>>>> bd9464615bb17117429880d2596ef04dbdd6f2e2
 
 ?>
