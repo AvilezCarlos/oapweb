dbf7e99a0b2fc875d162ce846fad90b858dd4531
Avilez Carlos <band_rockstar@hotmail.com>
2022-05-19 16:25:43 -0500

    Terminado 2

--- con_db.php
+++ con_db.php
@@ -1,6 +1,9 @@
 <?php 
-
-$conn = mysqli_connect("localhost","root","","oap");
+$localhost = "bsyffxelnsbjnvlulpoq-mysql.services.clever-cloud.com";
+$username = "ujznk78nqdfxhadg";
+$password = "FjcUrjdT37vwK82JTm4l";
+$database = "bsyffxelnsbjnvlulpoq";
+$conn = mysqli_connect($localhost,$username,$password,$database);
 
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
--- conexion.php
+++ conexion.php
@@ -1,10 +1,15 @@
 <?php 
+
 class Conexion{	  
-    public static function Conectar() {        
-        define('servidor', 'localhost');
-        define('nombre_bd', 'oap');
-        define('usuario', 'root');
-        define('password', '');					        
+    public static function Conectar() {   
+        $localhost = "bsyffxelnsbjnvlulpoq-mysql.services.clever-cloud.com";
+    $username = "ujznk78nqdfxhadg";
+    $password = "FjcUrjdT37vwK82JTm4l";
+    $database = "bsyffxelnsbjnvlulpoq";
+        define('servidor', $localhost);
+        define('nombre_bd', $database);
+        define('usuario', $username);
+        define('password', $password);					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
